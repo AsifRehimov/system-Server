afac4252dd8a1f4cc9407b9f213424cae39ce303
AsifRehimov <asifrhimov8@gmail.com>
2023-07-17 17:57:03 +0400

    update6

--- system-client/src/registr/Registr.js
+++ system-client/src/registr/Registr.js
@@ -5,6 +5,11 @@ import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import { RegistrDiv } from "./Registr.styled";
 import logo from "../photo/logo.png";
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+
 
 const Registr = () => {
 
@@ -16,51 +21,94 @@ const Registr = () => {
     const [bio, setBio] = useState('')
 
     const handleSubmit = () => {
-        axios.post('/registr',
-            {   fullName: fullName,
-                username: userName,
-                email: email,
-                password: password,
-                bio: bio,
-            }).then(res => {
-                return (
-                    res.status === 201 ?
-                        (navigate('/login'),
-                            localStorage.setItem("item", res.data))
-                        : alert("ad soyad sehvdir")
-                )
-            }).catch(err => alert(err))
+        if (fullName.length < 5) {
+            MySwal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Ad və Soyadınız 5 xarakterdən az ola bilməz.',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        } else if (userName.length < 5) {
+            MySwal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'İstifadəçi adınız 5 xarakterdən az ola bilməz.',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        } else if (email.length < 5) {
+            MySwal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Emailiniz 5 xarakterdən az ola bilməz.',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        } else if (password.length < 7) {
+            MySwal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Şifrəniz minimum 8 xarakter olmalıdır.',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        } else if (bio.length === 0) {
+            MySwal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Zəhmət olmasa məşğuliyyət əlavə edin. Məsələn "məktəbli"',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        } else {
+            axios.post('/registr',
+                {
+                    fullName: fullName,
+                    username: userName,
+                    email: email,
+                    password: password,
+                    bio: bio,
+                }).then(res => {
+                    return (
+                        res.status === 201 ?
+                            (navigate('/login'),
+                                localStorage.setItem("item", res.data))
+                            : alert("ad soyad sehvdir")
+                    )
+                }).catch(err => alert(err))
+        }
     }
     //   if (localStorage.item !== undefined) return navigate('/');
-
+ 
 
     return (
         <RegistrDiv>
             <div className="logo_div">
-                <img src={logo} alt="logo" className="logo"/>
+                <img src={logo} alt="logo" className="logo" />
                 <p>Chemistry</p>
             </div>
             <form className="form">
                 <p className="form_title">Qeydiyyat</p>
                 <div className='form-group'>
-                    <Input className="input" color="error"   labelPlaceholder="Ad, Soyad" onChange={(e) => setFullName(e.target.value)} />
+                    <Input className="input" color="error" labelPlaceholder="Ad, Soyad" onChange={(e) => setFullName(e.target.value)} />
                 </div>
                 <div className='form-group'>
-                    <Input className="input" color="error"  labelPlaceholder="İstifadəçi adı" onChange={(e) => setUserName(e.target.value)} />
+                    <Input className="input" color="error" labelPlaceholder="İstifadəçi adı" onChange={(e) => setUserName(e.target.value)} />
                 </div>
                 <div className='form-group'>
-                    <Input className="input" color="error"  labelPlaceholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                    <Input className="input" color="error" labelPlaceholder="Email" onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className='form-group'>
-                    <Input.Password className="input" color="error"  labelPlaceholder="Parol" initialValue={password} onChange={(e) => setPassword(e.target.value)} />
+                    <Input.Password className="input" color="error" labelPlaceholder="Parol" initialValue={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div className='form-group'>
-                    <Input className="input" color="error"  labelPlaceholder="Məşğuliyyət" onChange={(e) => setBio(e.target.value)} />
+                    <Input className="input" color="error" labelPlaceholder="Məşğuliyyət" onChange={(e) => setBio(e.target.value)} />
                 </div>
                 <Button className="inputButton" auto color="primary" rounded onClick={handleSubmit}>
                     Daxil ol
                 </Button>
-                <p className="passTo">Hesabınəz var? O zaman yubanmadan daxil ol. {<Link className="link" to={"/registration"}>Daxil ol</Link>}</p>
+                <p className="passTo">Hesabınəz var? O zaman yubanmadan daxil ol. {<Link className="link" to={"/login"}>Daxil ol</Link>}</p>
             </form>
         </RegistrDiv>
     )
